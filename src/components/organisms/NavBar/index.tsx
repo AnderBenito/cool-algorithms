39cf01369dc9003d50e190e8c93ebc139f8cfba9
Ander Benito <anderbenito96@gmail.com>
2020-12-19 15:37:25 +0100

    Fixed navbar centering

--- src/components/organisms/NavBar/index.tsx
+++ src/components/organisms/NavBar/index.tsx
@@ -1,15 +1,23 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Link as ChakraLink, Text } from "@chakra-ui/react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar: React.FC = () => {
 	return (
 		<Flex as="nav" bg="teal.500" padding="1.5rem">
 			<Flex align="center" mr={5}>
-				<Heading as="a">Cool Algo</Heading>
+				<Heading cursor="pointer">
+					<Link to="/">Cool Algo</Link>
+				</Heading>
 			</Flex>
-			<Box>
-				<Text>Sorting</Text>
-			</Box>
+
+			<Flex align="center">
+				<Text>
+					<ChakraLink as={Link} to="/sorting">
+						Sorting
+					</ChakraLink>
+				</Text>
+			</Flex>
 		</Flex>
 	);
 };
