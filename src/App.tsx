9c15186d831a412c01420d34c8f7cce8af3b5094
Ander Benito <anderbenito96@gmail.com>
2020-12-19 15:36:08 +0100

    Fixed route provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from "./components/organisms/NavBar";
 import Routes from "./Routes";
 
 const App: React.FC = () => {
 	return (
-		<>
+		<Router>
 			<NavBar />
 			<Routes />
-		</>
+		</Router>
 	);
 };
 
