e1afa89ef75d23aad47e7734b0a06682d3bae695
Ander Benito <anderbenito96@gmail.com>
2020-12-20 23:02:44 +0100

    Removed separate sorting views

--- src/views/SortingView/MergeSortView/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import SortingControllerContainer from "../../../containers/SortingControllerContainer";
-import mergeSort from "../../../utils/algorithms/sorting/mergeSort";
-
-const MergeSortView: React.FC = () => {
-	return <SortingControllerContainer sortingAlgorithm={mergeSort} />;
-};
-
-export default MergeSortView;
--- src/views/SortingView/index.tsx
+++ src/views/SortingView/index.tsx
@@ -1,19 +1,8 @@
 import React from "react";
-import { Route } from "react-router-dom";
 import SortingContainer from "../../containers/SortingContainer";
-import BubbleSortView from "./BubbleSortView";
-import InsertionSortView from "./InsertionSortView";
-import MergeSortView from "./MergeSortView";
 
 const SortingView: React.FC = () => {
-	return (
-		<>
-			<SortingContainer />
-			<Route path="/sorting/bubble" component={BubbleSortView} />
-			<Route path="/sorting/insertion" component={InsertionSortView} />
-			<Route path="/sorting/merge" component={MergeSortView} />
-		</>
-	);
+	return <SortingContainer />;
 };
 
 export default SortingView;
