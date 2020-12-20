fbc449403ef98e3f8edf9a369a9440a7791144fc
Ander Benito <anderbenito96@gmail.com>
2020-12-20 23:15:25 +0100

    Removed unused containers

--- src/containers/BubbleSortContainer/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import SortingControllerContainer from "../SortingControllerContainer";
-import bubbleSort from "../../utils/algorithms/sorting/bubbleSort";
-
-const BubbleSortContainer: React.FC = () => {
-	return <SortingControllerContainer sortingAlgorithm={bubbleSort} />;
-};
-
-export default BubbleSortContainer;
