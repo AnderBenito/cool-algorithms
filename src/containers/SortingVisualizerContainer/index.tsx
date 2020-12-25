185baa1eef25e3f49a1b52d988182a92a35a73b5
Ander Benito <anderbenito96@gmail.com>
2020-12-25 11:01:21 +0100

    Modified visualizer container

--- src/containers/SortingVisualizerContainer/index.tsx
+++ src/containers/SortingVisualizerContainer/index.tsx
@@ -1,22 +1,26 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
 import Bar from "../../components/atoms/Bar";
-import { Trace } from "../../utils/algorithms/sorting/helpers";
-import styles from "./index.module.css";
+import { Trace } from "../../utils/helpers";
 
 interface Props {
 	animation: Trace;
 }
 
 const SortingVisualizerContainer: React.FC<Props> = ({ animation }) => {
+	if (!animation) return <Box>Error</Box>;
 	return (
 		<Box
 			bg={"gray.400"}
-			padding="0 16px"
-			className={styles["visualizerContainer"]}
+			height="500px"
+			display="flex"
+			flexDirection="column"
+			justifyContent="space-between"
 		>
-			{animation &&
-				animation.state.map((value, index) => {
+			<Box>Array accesses: {animation.accesses}</Box>
+			<Box>Array comparisons: {animation.comparisons}</Box>
+			<Box display="flex" alignItems="flex-end" justifyContent="space-between">
+				{animation.state.map((value, index) => {
 					let isComparing = animation.compare.includes(index);
 					let isSwapping = animation.swap.includes(index);
 					return (
@@ -28,6 +32,7 @@ const SortingVisualizerContainer: React.FC<Props> = ({ animation }) => {
 						></Bar>
 					);
 				})}
+			</Box>
 		</Box>
 	);
 };
